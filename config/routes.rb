a4d2f3b15d0b5f15a080badb074ca5001425feb8
solene basaia <bsolene@hotmail.fr>
2017-05-22 16:26:06 +0100

    implement word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'logins/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/game', to: 'word#game'
+
+get '/score', to: 'word#score'
+
+get '/', to: 'logins#create'
+
+
 end
